2a5e77887c329714e39c8913c89594fe1b8c5e95
Nikoleta Kosareva <nickoleta@kosareva.ru>
2023-09-02 11:43:37 +0700

    Вывод страницы лабораторной

--- app.py
+++ app.py
@@ -3,5 +3,23 @@ app=Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на Flask"
-    
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title> Косарева Николета Вячеславовна, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа №1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Косарева Николета, ФБИ-12, 3 курс, 2023
+        </footer>
+    </body>
+</html> 
+"""
+

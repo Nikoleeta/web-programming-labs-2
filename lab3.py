9fe6f301785853a4a81835988362e69c7fdf67c7
nikoleta <nikoletaganenko@ya.ru>
2023-10-12 20:05:05 +0700

    Ticket

--- lab3.py
+++ lab3.py
@@ -48,3 +48,51 @@ def pay():
 @lab3.route('/lab3/success')
 def suc():
     return render_template('success.html')
+
+
+@lab3.route('/lab3/ticket')
+def tick():
+    errors={}
+
+    user=request.args.get('user')
+    if user == '':
+        errors['user']='Fill in the field'
+
+    type=request.args.get('type')
+    if type == '':
+        errors['type']='Fill in the field'
+
+    seat=request.args.get('seat')
+    if seat == '':
+        errors['seat']='Fill in the field'
+
+    lugagge=request.args.get('lugagge')
+    if lugagge == '':
+        errors['lugagge']='Fill in the field'
+
+    age=request.args.get('age') 
+    if age == '':
+        errors['age']='Fill in the field'
+
+    date=request.args.get('date')
+    if date == '':
+        errors['date']='Fill in the field'
+
+    departure=request.args.get('departure')
+    if departure == '':
+        errors['departure']='Fill in the field'
+
+    destination=request.args.get('destination')
+    if destination == '':
+        errors['destination']='Fill in the field'
+
+    
+    return render_template('ticket.html', errors=errors, user=user,
+                          type=type, seat=seat, lugagge=lugagge,
+                          age=age, date=date,departure=departure,
+                          destination=destination)
+
+
+@lab3.route('/lab3/buy')
+def ticket():
+    return render_template('ticketbuy.html')

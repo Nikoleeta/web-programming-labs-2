99d2774c6f6cd31612db45d21e77993f83c65e59
Nikoleta Kosareva <nickoleta@kosareva.ru>
2023-11-17 19:23:52 +0700

    'vse'

--- lab6.py
+++ lab6.py
@@ -8,9 +8,11 @@ lab6=Blueprint('lab6', __name__)
 
 @lab6.route('/lab6')
 def main():
-    my_users=users.query.all()
-    print(my_users)
-    return 'result in console'
+    if current_user.is_authenticated:
+        username = current_user.username
+    else:
+        username = "Аноним"
+    return render_template('lab6.html', username=username)
 
 @lab6.route('/lab6/checkarticles')
 def check():
@@ -83,4 +85,43 @@ def login():
 @login_required
 def articles_list():
     my_articles=articles.query.filter_by(user_id=current_user.id).all()
-    return render_template('note2.html', articles=my_articles)
+    return render_template('article_list.html', articles=my_articles)
+
+
+@lab6.route('/lab6/articles/<int:article_id>')
+@login_required
+def view_article(article_id):
+    article = articles.query.get(article_id)
+    return render_template('article_list.html', article=article)
+
+@lab6.route('/lab6/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/lab6')
+
+
+@lab6.route('/lab6/articles/add', methods=['GET', 'POST'])
+@login_required
+def add_article():
+    if request.method == 'POST':
+        title_article = request.form['title_article']
+        text_article = request.form['text_article']
+        new_article = articles(title=title_article, article_text=text_article, user_id=current_user.id)
+        db.session.add(new_article)
+        db.session.commit()
+        return redirect('/lab6/articles')
+    return render_template('note.html')
+
+# @lab6.route('/lab6/article/<int:article_id>')
+# @login_required
+# def view_article(article_id):
+#     article_id = articles.query.get(article_id)
+#     article_text = articles.query.get(article_text)
+#     if article:
+#         return render_template('note2.html', article_text=article_text)
+#     else: 
+#         return "Статья не найдена"
+
+
+

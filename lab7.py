63dfd40fda8e4eb22a1ffdd1a14e3d5a08bc383c
nikoleta <nikoletaganenko@ya.ru>
2023-12-06 23:19:57 +0700

    1

--- lab7.py
+++ lab7.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, make_response
+from flask import abort
 lab7=Blueprint('lab7', __name__)
 
 @lab7.route('/lab7/')
@@ -22,35 +23,34 @@ def api():
     
     abort(400)
 
-    def get_price(params):
-        return{'result': calculate_price(params), 'error':None}
+def get_price(params):
+    return{'result': calculate_price(params), 'error':None}
     
-    def calculate_price(params):
-        drink=params['drink']
-        milk=params['milk']
-        sugar=params['sugar']
+def calculate_price(params):
+    drink=params['drink']
+    milk=params['milk']
+    sugar=params['sugar']
+    if drink == 'coffee':
+        price = 120
+    elif drink == 'black tea':
+        price=80
+    else:
+        price=70
 
-        if drink == 'coffee':
-            price = 120
-        elif drink == 'black tea':
-            price=80
-        else:
-            price=70
+    if milk:
+        price+=30
+    if sugar:
+        price+=10
 
-        if milk:
-            price+=30
-        if sugar:
-            price+=10
-
-        return price
+    return price
     
-    def pay(params):
-        card_num=params['card_num']
-        if len(card_num) != 16 or not card_num.isdigit():
-            return {'result':None, "error": 'Invalid number'}
-        cvv=params['cvv']
-        if len(cvv) != 3 or not cvv.isdigit():
-            return {'result':None, "error": 'Invalid number cvv'}
+def pay(params):
+    card_num=params['card_num']
+    if len(card_num) != 16 or not card_num.isdigit():
+        return {'result':None, "error": 'Invalid number'}
+    cvv=params['cvv']
+    if len(cvv) != 3 or not cvv.isdigit():
+        return {'result':None, "error": 'Invalid number cvv'}
         
-        price=calculate_price(params)
-        return{'result': f'С карты {card_num} списано {price} руб', 'error': None}
+    price=calculate_price(params)
+    return{'result': f'С карты {card_num} списано {price} руб', 'error': None}

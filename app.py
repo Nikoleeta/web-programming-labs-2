132f039996e4e9000b54ef57c502645479fbb3b5
Nikoleta Kosareva <nickoleta@kosareeva.ru>
2023-12-21 22:31:10 +0700

    1

--- app.py
+++ app.py
@@ -11,10 +11,12 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 
 
 
 app=Flask(__name__)
+
 app.secret_key='123'
 
 user_db='nikoleta_knowledge_base_orm'
@@ -46,3 +48,4 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
